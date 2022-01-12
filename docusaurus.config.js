f3a460a48ec5bffaa12af278c7039811aa32af99
Ruskin Janowski <ruskin.janowski@gmail.com>
2022-01-12 20:26:12 +0200

    Remove docs in url and server at root

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,16 +24,12 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+		  routeBasePath: '/', // Serve the docs at the site's root
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/metaviewio/metaview-docs',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/metaviewio/metaview-docs',
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
