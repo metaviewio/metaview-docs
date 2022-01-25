4eb3abdcbb36f3f070dfa6aa32c6c0de8ea8c084
Mikhail Janowski <mikhail.janowski@gmail.com>
2022-01-25 09:36:45 +0200

    Configure Google Analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,6 +33,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-LDPB34BHFW',
+          anonymizeIP: false,
+        },
       }),
     ],
   ],
